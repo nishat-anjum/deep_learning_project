63dcfe4cc175723e485564fa601946000229e071
nishat-anjum <nanjum.du14@gmail.com>
2024-12-16 00:53:58 +0600

    Added audio splitting functinality

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
 DOWNLOAD_DIRECTORY = os.path.join(os.path.dirname(os.getcwd()), "audios")
-
 CSV_FILE_PATH = os.path.join(os.getcwd(), "platlist.csv")
+WAV_FILE_DIRECTORY = os.path.join(os.path.dirname(os.getcwd()), "wav_files")
+AUDIO_SPLIT_DURATION = 15 * 1000
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from data_processing import audio_spliter
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    audio_spliter.split_audio()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
